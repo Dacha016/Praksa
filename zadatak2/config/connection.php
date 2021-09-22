a6f2e65408c45ee6385abdc12e7f945b7b1b8e77
Dacha016 <dalibor.marinkovic@gmail.com>
2021-09-22 10:41:21 +0200

    prazan fajl mentor.php (test)

---
+++ zadatak2/config/connection.php
@@ -0,0 +1,24 @@
+<?php
+class Database {
+    private $host ="localhost";
+    private $user="root";
+    private  $pass="";
+    private  $dbName="praksa";
+    private  $conn;
+   
+    public function connect(){
+        $this->conn=null;
+       
+        try{
+            $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->dbName, $this->user,$this->pass);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            
+        }catch(Exception $e){
+            return false;
+        }
+        return $this->conn;
+        
+    } 
+    
+}
+?>
